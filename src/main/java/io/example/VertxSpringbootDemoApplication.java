50dd1aee055f8300a9f8d7a35e8b8c3c386609d4
ian <jasterx@qq.com>
2018-03-12 18:15:05 +0800

    Initial commit

---
+++ src/main/java/io/example/VertxSpringbootDemoApplication.java
@@ -0,0 +1,91 @@
+package io.example;
+
+import io.example.routers.BookRestApi;
+import io.example.vertx.SpringVerticleFactory;
+import io.example.vertx.SpringWorker;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+@SpringBootApplication
+public class VertxSpringbootDemoApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(VertxSpringbootDemoApplication.class);
+
+    @Autowired
+    SpringVerticleFactory verticleFactory;
+
+    /**
+     * The Vert.x worker pool size, configured in the {@code application.properties} file.
+     * <p>
+     * Make sure this is greater than {@link #springWorkerInstances}.
+     */
+    @Value("${vertx.worker.pool.size}")
+    int workerPoolSize;
+
+    /**
+     * The number of {@link SpringWorker} instances to deploy, configured in the {@code application.properties} file.
+     */
+    @Value("${vertx.springWorker.instances}")
+    int springWorkerInstances;
+
+    public static void main(String[] args) {
+        SpringApplication.run(VertxSpringbootDemoApplication.class, args);
+    }
+
+    @EventListener
+    public void deployVerticles(ApplicationReadyEvent event) {
+        Vertx vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(workerPoolSize));
+
+        // The verticle factory is registered manually because it is created by the Spring container
+        vertx.registerVerticleFactory(verticleFactory);
+
+        CountDownLatch deployLatch = new CountDownLatch(2);
+        AtomicBoolean failed = new AtomicBoolean(false);
+        String restApiVerticleName = verticleFactory.prefix() + ":" + BookRestApi.class.getName();
+        vertx.deployVerticle(restApiVerticleName, ar -> {
+            if (ar.failed()) {
+                logger.error("Failed to deploy verticle", ar.cause());
+                failed.compareAndSet(false, true);
+            }
+            deployLatch.countDown();
+        });
+
+        DeploymentOptions workerDeploymentOptions = new DeploymentOptions()
+                .setWorker(true)
+                // As worker verticles are never executed concurrently by Vert.x by more than one thread,
+                // deploy multiple instances to avoid serializing requests.
+                .setInstances(springWorkerInstances);
+        String workerVerticleName = verticleFactory.prefix() + ":" + SpringWorker.class.getName();
+        vertx.deployVerticle(workerVerticleName, workerDeploymentOptions, ar -> {
+            if (ar.failed()) {
+                logger.error("Failed to deploy verticle", ar.cause());
+                failed.compareAndSet(false, true);
+            }
+            deployLatch.countDown();
+        });
+
+        try {
+            if (!deployLatch.await(10, SECONDS)) {
+                throw new RuntimeException("Timeout waiting for verticle deployments");
+            } else if (failed.get()) {
+                throw new RuntimeException("Failure while deploying verticles");
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
